9d22e8a53be8681d98451dcfbc91ce9947ab2fd4
Wandonium <hillarywando@gmail.com>
2023-01-25 20:36:11 +0300

    Added function for matching numbers in input string.

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,25 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("trivialMatcher result: " + trivialMatcher());
+        System.out.println("numberMatcher result: " + numberMatcher());
+    }
+
+    public static boolean trivialMatcher() {
         // pattern to look for in input string
         Pattern pattern = Pattern.compile("hello");
 
         // input string to look for pattern in
-        Matcher matcher = pattern.matcher("hell1o");
+        Matcher matcher = pattern.matcher("hello");
 
-        boolean found = matcher.find();
-        System.out.println("found: " + found);
+        // return boolean value; true if pattern matches; false otherwise
+        return matcher.find();
+    }
+
+    public static boolean numberMatcher() {
+        Pattern pattern = Pattern.compile("agent \\d\\d\\d");
+        Matcher matcher = pattern.matcher("agent 007");
+        return matcher.find();
     }
 }
