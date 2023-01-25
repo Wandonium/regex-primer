1073c135d2202c6e3062676c79976db9dcd9560a
Wandonium <hillarywando@gmail.com>
2023-01-25 21:40:36 +0300

    Added code to show how to replace a part of the input string with a given string after matching it.

--- src/ExtractingStrings.java
+++ src/ExtractingStrings.java
@@ -14,5 +14,24 @@ public class ExtractingStrings {
             System.out.println("Group: " + matcher.group());
             System.out.println("First Group: " + matcher.group(1));
         }
+        matchCard();
+        replaceCard();
+    }
+
+    public static void matchCard() {
+//        Pattern pattern = Pattern.compile("^\\d{4}-\\d{4}-\\d{4}-\\d{4}$");
+        Pattern pattern = Pattern.compile("^(\\d{4}-){3}\\d{4}$");
+        Matcher matcher = pattern.matcher("1234-2222-3333-4343");
+        System.out.println("Found card number: " + matcher.find());
+    }
+
+    public static void replaceCard() {
+        // replace card number 1234-2222-3333-4343 with
+        // XXXX-XXXX-XXXX-4343
+        Pattern pattern = Pattern.compile("\\d{4}-\\d{4}-\\d{4}-");
+        Matcher matcher = pattern.matcher("1234-2222-3333-4343");
+
+        String maskedCardNumber = matcher.replaceAll("XXXX-XXXX-XXXX-");
+        System.out.println("Masked card num: " + maskedCardNumber);
     }
 }
