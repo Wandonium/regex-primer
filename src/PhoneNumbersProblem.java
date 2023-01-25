40f091b10db2a198399c20731eb4fd447a6ae0e9
Wandonium <hillarywando@gmail.com>
2023-01-25 23:15:31 +0300

    Added code for matching different Kenyan phone number configurations.

---
+++ src/PhoneNumbersProblem.java
@@ -0,0 +1,18 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PhoneNumbersProblem {
+
+    // Problem: matching different Kenyan
+    // phone number input configurations
+    public static void main(String[] args) {
+        String phoneNumbers = "+254708072998\n\n0708072998\n\n708072998\n\n01108072998";
+
+        Pattern pattern = Pattern.compile("^([+]2547|07|7|011)\\d{8}$", Pattern.MULTILINE);
+        Matcher matcher = pattern.matcher(phoneNumbers);
+        int count = 0;
+        while(matcher.find()) count++;
+        System.out.println("Phone Numbers: " + phoneNumbers);
+        System.out.println("No. of matches: " + count);
+    }
+}
